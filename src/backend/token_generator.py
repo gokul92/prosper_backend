b3abac295e62c26abccbbc35ca7647ecc52455f7
gokul <gokul.er137@gmail.com>
2024-07-23 22:21:51 -0700

    functions to encrypt and decrypt any id based on key.

--- src/backend/token_generator.py
+++ src/backend/token_generator.py
@@ -1,32 +1,17 @@
 import os
-import uuid
 from dotenv import load_dotenv
 from cryptography.fernet import Fernet
 
-
-def generate_encrypted_token():
-    load_dotenv()
-    uuid_token = uuid.uuid4()
-    fernet_key = bytes(os.getenv('CRYPTO_KEY'), "utf-8")
-    fernet_instance = Fernet(fernet_key)
-    encrypted_token = fernet_instance.encrypt(bytes(str(uuid_token), 'utf-8'))
-    return encrypted_token
+load_dotenv()
+fernet_key = bytes(os.getenv('CRYPTO_KEY'), "utf-8")
+fernet_instance = Fernet(fernet_key)
 
 
-def decrypt_token(encrypted_token: bytes):
-    load_dotenv()
-    fernet_key = os.getenv('CRYPTO_KEY')
-    fernet_instance = Fernet(fernet_key)
-    decrypted_token = fernet_instance.decrypt(encrypted_token)
-    return decrypted_token
+def encrypt_id(id_string):
+    """Encrypt any ID (session or thread) to use as a token"""
+    return fernet_instance.encrypt(id_string.encode()).decode()
 
 
-def match_encrypted_token(encrypted_token: bytes):
-    """
-    :param encrypted_token:
-    :return: bool
-    """
-    decrypted_token = decrypt_token(encrypted_token)
-    token_match = "check if decrypted token exists in db. if true, then return True. else False"
-    return token_match
-
+def decrypt_id(encrypted_id):
+    """Decrypt the token to get the original ID"""
+    return fernet_instance.decrypt(encrypted_id.encode()).decode()
