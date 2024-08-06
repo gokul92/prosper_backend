de7fbe599cb5428cb315bcdb4c963a83b4a0667d
gokul <gokul.er137@gmail.com>
2024-08-06 10:30:35 -0700

    1. support fetching and persisting equity price history
    2. support user arguments to customize data fetching

--- src/etl/morningstar_equity_data.py
+++ src/etl/morningstar_equity_data.py
@@ -11,6 +11,7 @@ import concurrent.futures
 import time
 import logging
 import csv
+from functools import partial
 
 load_dotenv()
 
@@ -98,6 +99,27 @@ def init_db():
                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
             )
             """)
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS equities_daily_price_history (
+                date DATE NOT NULL,
+                open_price DOUBLE PRECISION,
+                high_price DOUBLE PRECISION,
+                low_price DOUBLE PRECISION,
+                close_price DOUBLE PRECISION,
+                volume DOUBLE PRECISION,
+                ticker TEXT NOT NULL,
+                company_name TEXT,
+                as_of_date DATE NOT NULL,
+                created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                CONSTRAINT pk_equities_daily_price_history PRIMARY KEY (date, ticker, as_of_date)
+            );
+            
+            -- Create indexes for faster querying
+            CREATE INDEX IF NOT EXISTS idx_equities_daily_price_history_ticker ON equities_daily_price_history(ticker);
+            CREATE INDEX IF NOT EXISTS idx_equities_daily_price_history_as_of_date ON equities_daily_price_history(as_of_date);
+            CREATE INDEX IF NOT EXISTS idx_equities_daily_price_history_created_at ON equities_daily_price_history(created_at);
+
+            """)
             conn.commit()
 
 
@@ -113,10 +135,45 @@ def add_to_db(df, table_name):
 
 
 def get_symbol_guide():
-    SYMBOL_GUIDE_URL = f"http://mshxml.morningstar.com/symbolGuide?username={mstar_symbol_guide_username}&password={mstar_symbol_guide_password}&exchange=126&security=1&JSONShort"
-    response = requests.get(SYMBOL_GUIDE_URL)
+    symbol_guide_url = f"http://mshxml.morningstar.com/symbolGuide?username={mstar_symbol_guide_username}&password={mstar_symbol_guide_password}&exchange=126&security=1&JSONShort"
+    response = requests.get(symbol_guide_url)
     df = pd.DataFrame(response.json()[ 'quotes' ][ 'results' ])
     return df[ [ 'H1', 'S12', 'S19', 'S1735', 'S1736', 'S3059' ] ].to_dict(orient='records')
+
+
+def get_market_price_history(security, start_date, end_date, as_of_date):
+    mkt_price_params = {
+        'exchangeId': security[ 'S1736' ],
+        "identifierType": "Symbol",
+        "identifier": security[ "H1" ],
+        "startDate": start_date,
+        "endDate": end_date
+    }
+    # print(start_date, end_date, as_of_date)
+    url = f"{BASE_URL}GlobalStockPricesService.asmx/GetEODPriceHistory"
+    response = requests.get(url, params={**EQUITY_API_PARAMS, **mkt_price_params,
+                                         "category": "GetEODPriceHistory"})
+    # response.raise_for_status()  # This will raise an exception for HTTP errors
+    # logging.info(f"Received response for {security[ 'H1' ]}")
+    # print(f"Response for {security[ 'H1' ]}:", response.text)  # Print raw response
+    if response.json()[ 'MessageInfo' ][ 'MessageCode' ] == 200:
+        company_name = response.json()[ 'GeneralInfo' ][ 'CompanyName' ]
+        data = response.json()[ 'EODPriceEntityList' ]
+        if data:
+            result = pd.DataFrame(data)
+            result.drop([ 'PriceCurrencyId' ], axis=1, inplace=True)
+            result[ 'Symbol' ] = security[ 'H1' ]
+            result[ 'CompanyName' ] = company_name
+            result.dropna(axis=0, subset='Symbol', inplace=True)
+            result[ 'as_of_date' ] = as_of_date
+            result.columns = [ 'date', 'open_price', 'high_price', 'low_price', 'close_price', 'volume', 'ticker',
+                               'company_name', 'as_of_date' ]
+            add_to_db(result, 'equities_daily_price_history')
+    return None
+
+
+def get_market_capitalization(security):
+    pass
 
 
 def get_dividend_yield(security):
@@ -129,9 +186,8 @@ def get_dividend_yield(security):
     url = f"{BASE_URL}FinancialKeyRatiosService.asmx/GetValuationRatios"
     response = requests.get(url, params={**EQUITY_API_PARAMS, **div_yield_params,
                                          "category": "GetValuationRatios"})
-
-    if response.json()['MessageInfo']['MessageCode'] == 200:
-        company_name = response.json()[ 'GeneralInfo' ]['CompanyName']
+    if response.json()[ 'MessageInfo' ][ 'MessageCode' ] == 200:
+        company_name = response.json()[ 'GeneralInfo' ][ 'CompanyName' ]
         data = response.json()[ 'ValuationRatioEntityList' ]
 
         if data:
@@ -255,7 +311,7 @@ def fetch_equity_performance(symbols, start_date, end_date, as_of_date):
             return cur.fetchall()
 
 
-def main(eq_perf_bool, eq_ind_bool, eq_yield_bool, as_of_date, num_workers=25):
+def main(eq_perf_bool, eq_ind_bool, eq_yield_bool, eq_price_hist_bool, num_workers, as_of_date, start_date, end_date):
     init_db()
     symbol_guide_mstar = get_symbol_guide()
     if eq_ind_bool:
@@ -277,7 +333,7 @@ def main(eq_perf_bool, eq_ind_bool, eq_yield_bool, as_of_date, num_workers=25):
         start_time_dividend_yield = time.time()
         with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
             dividend_yield_results = list(executor.map(get_dividend_yield, symbol_guide_mstar))
-            dividend_yield_results = [d for d in dividend_yield_results if d != {}]
+            dividend_yield_results = [ d for d in dividend_yield_results if d != {} ]
             dividend_yield_df = pd.DataFrame(dividend_yield_results).dropna(subset='Symbol')
             dividend_yield_df[ 'as_of_date' ] = as_of_date
             dividend_yield_df.columns = [ 'symbol', 'company_name', 'date', 'dividend_yield', 'as_of_date' ]
@@ -287,10 +343,23 @@ def main(eq_perf_bool, eq_ind_bool, eq_yield_bool, as_of_date, num_workers=25):
         log_to_csv('Yield Classification', duration)
         logging.info(f"Yield classification completed in {duration:.2f} seconds")
 
+    if eq_price_hist_bool and start_date is not None and end_date is not None:
+        # print("entered price history")
+        start_time_price_hist = time.time()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
+            # executor.map(get_market_price_history, symbol_guide_mstar, start_date, end_date, as_of_date)
+            partial_func = partial(get_market_price_history, start_date=start_date, end_date=end_date,
+                                   as_of_date=as_of_date)
+            executor.map(partial_func, symbol_guide_mstar)
+        end_time_price_hist = time.time()
+        duration = end_time_price_hist - start_time_price_hist
+        log_to_csv('Price History', duration)
+        logging.info(f"Price history completed in {duration:.2f} seconds")
+
     if eq_perf_bool:
         start_time_equity_performance = time.time()
         with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-            list(executor.map(process_security, symbol_guide_mstar, as_of_date))
+            executor.map(process_security, symbol_guide_mstar, as_of_date)
         end_time_equity_performance = time.time()
         duration = end_time_equity_performance - start_time_equity_performance
         log_to_csv('Equity Performance', duration)
@@ -298,23 +367,38 @@ def main(eq_perf_bool, eq_ind_bool, eq_yield_bool, as_of_date, num_workers=25):
 
 
 if __name__ == "__main__":
-    opts, args = getopt.getopt(sys.argv[ 1: ], "piydw:", [ "performance", "industry", "yield", "date=", "workers=" ])
-    eqpb = False
-    eqib = False
-    eqyb = False
-    dt = date.today().strftime('%Y-%m-%d')
-    nw = 25
+    opts, args = getopt.getopt(sys.argv[ 1: ], "piyhw:d:e:a:",
+                               [ "performance", "industry", "yield", "history", "workers=", "date=", "end_date=",
+                                 "days=" ])
+    perf = False
+    industry = False
+    yld = False
+    history = False
+    n_workers = 25
+    as_of_date = date.today().strftime('%Y-%m-%d')
+    start_date = None
+    end_date = None
+    num_days = None
 
     for opt, arg in opts:
         if opt in ("-p", "--performance"):
-            eqpb = True
+            perf = True
         elif opt in ("-i", "--industry"):
-            eqcb = True
+            industry = True
         elif opt in ("-y", "--yield"):
-            eqyb = True
-        elif opt in ("-d", "--date"):
-            dt = arg
+            yld = True
+        elif opt in ("-h", "--history"):
+            history = True
         elif opt in ("-w", "--workers"):
-            nw = int(arg)
+            n_workers = int(arg)
+        elif opt in ("-d", "--date"):
+            as_of_date = arg
+        elif opt in ("-e", "--end_date"):
+            end_date = arg
+        elif opt in ("-a", "--days"):
+            num_days = int(arg)
+            if end_date is not None:
+                end_date_obj = datetime.strptime(end_date, "%Y-%m-%d")
+                start_date = (end_date_obj - timedelta(days=num_days)).strftime("%Y-%m-%d")
 
-    main(eqpb, eqib, eqyb, nw)
+    main(perf, industry, yld, history, n_workers, as_of_date, start_date, end_date)
